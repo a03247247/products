ca3e75e85d606362bb602cf2cc1f63768fa036b8
Tsai <a03247247@gmail.com>
2021-05-10 23:27:52 +0800

    新增欄位名稱,並調整編碼

--- products.py
+++ products.py
@@ -4,6 +4,8 @@ while True:
     if name == 'q':  #quit離開
         break
     price = input('請輸入商品價格')
+
+    
     p =[]
     p.append(name)
     p.append(price)    #也可以寫成
@@ -13,6 +15,7 @@ print(products)
 for p in products:
     print(p[0],'的價格是',p[1])
 
-with open('products.csv', 'w') as f:
-    for a in products:
+with open('products.csv', 'w',encoding='utf-8') as f:
+    f.write('商品,價錢\n')
+    for p in products:
         f.write(p[0] + ',' + p[1] + '\n')
