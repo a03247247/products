26d391a8aa4cb23ecf1eceaa0a0cc02992ce8af5
Tsai <a03247247@gmail.com>
2021-05-18 21:56:03 +0800

    最終版本 建立funtion

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 "# products" 
+這是我寫的第一個完整程式
+裡面架設了funtion
+此程式可輸入商品名稱及價格
+並輸出成CSV檔 (coding = 'utf-8')
--- products.py
+++ products.py
@@ -1,37 +1,51 @@
 import os
 
-products = []
-
-if os.path.isfile('products.csv'):
-    print('讀取成功')
-    #讀取檔案
-    
-    with open('products.csv', 'r', encoding = 'utf-8') as f:
+def read_file(filename):
+    products = []       
+    with open(filename, 'r', encoding = 'utf-8') as f:
         for line in f:
             if '商品,價錢' in line:
                 continue  #繼續(跳過本次迴圈)
             name, price = line.strip().split(',')
             products.append([name, price])
     print(products)
-else:
-    print('讀取失敗')
+    return products    
 
-while True:
-    name = input('請輸入商品名稱')
-    if name == 'q':  #quit離開    
-        break
-    price = input('請輸入商品價格')
-    
-    p =[]
-    p.append(name)
-    p.append(price)    #也可以寫成
-    products.append(p) #products.append([name,price])
-print(products)
+def uese_input(products):
+    while True:
+        name = input('請輸入商品名稱(輸入"q"離開)')
+        if name == 'q':  #quit離開    
+            break
+        price = input('請輸入商品價格')        
+        p =[]
+        p.append(name)
+        p.append(price)    #也可以寫成
+        products.append(p) #products.append([name,price])
+    print(products)
+    return products
 
-for p in products:
-    print(p[0],'的價格是',p[1])
-
-with open('products.csv', 'w',encoding='utf-8') as f:
-    f.write('商品,價錢\n')
+def print_products(products):
     for p in products:
-        f.write(p[0] + ',' + p[1] + '\n')
+        print(p[0],'的價格是',p[1])
+       
+
+def write_file(filename, products):
+    with open(filename, 'w',encoding='utf-8') as f:
+        f.write('商品,價錢\n')
+        for p in products:
+            f.write(p[0] + ',' + p[1] + '\n')
+
+
+def main():
+    filename= 'products.csv'
+    if os.path.isfile(filename):
+        print('讀取成功')
+        products = read_file(filename)
+    else:
+         print('讀取失敗')
+
+    products = uese_input(products)
+    print_products(products)
+    write_file('products.csv', products)
+
+main()
